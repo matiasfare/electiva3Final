ee9b183166bdacd0f8d56e5a1eb118858fbbd8b4
Matias Fare <matiasfare59@gmail.com>
2020-11-05 21:19:18 -0300

    Proyecto base, maven sprint boot

---
+++ src/main/java/com/github/electica3Final/Electica3FinalApplication.java
@@ -0,0 +1,13 @@
+package com.github.electica3Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Electica3FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Electica3FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/electica3Final/Electica3FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.electica3Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Electica3FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
