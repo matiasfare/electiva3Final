0336ac1e5f1a3e6cc57fc0cd53a753d61c49e1eb
Matias Fare <matiasfare59@gmail.com>
2020-11-07 03:12:43 -0300

    Agrega entity Hospital-Servicio

---
+++ src/main/java/com/github/electica3Final/entities/HospitalServicio.java
@@ -0,0 +1,25 @@
+package com.github.electica3Final.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hospital_servicio", schema = "public")
+@Getter
+@Setter
+public class HospitalServicio {
+
+    @Id
+    @SequenceGenerator(name = "hospital_servicio_id_seq", sequenceName = "hospital_servicio_id_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "hospital_servicio_id_seq")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "id_servicio")
+    private Long idServicio;
+
+    @Column(name = "id_hospital")
+    private Long idHospital;
+}
